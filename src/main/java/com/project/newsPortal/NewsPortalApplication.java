b370919825b6a67cbf4148d701e75845ac31b1b1
Sarmad M. Munir <sarmad1122@gmail.com>
2023-03-12 11:31:14 +0500

    version 3.0

--- src/main/java/com/project/newsPortal/NewsPortalApplication.java
+++ src/main/java/com/project/newsPortal/NewsPortalApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewsPortalApplication {
+public class
+
+NewsPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsPortalApplication.class, args);
---
+++ src/main/java/com/project/newsPortal/entity/News.java
@@ -0,0 +1,4 @@
+package com.project.newsPortal.entity;
+
+public class News {
+}
